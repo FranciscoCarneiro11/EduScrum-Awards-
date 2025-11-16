371605cf26d6056c14ea54c39700a327d93d4062
FranciscoCarneiro11 <franciscomcarneiro2013@gmail.com>
2025-11-16 15:54:07 +0000

    Atualização Admin

--- backend/awards/awards/src/main/java/com/eduscrum/awards/model/Admin.java
+++ backend/awards/awards/src/main/java/com/eduscrum/awards/model/Admin.java
@@ -1,11 +1,8 @@
 package com.eduscrum.awards.model;
 
-<<<<<<< HEAD
-import com.fasterxml.jackson.annotation.JsonIgnoreType;
 
-=======
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
->>>>>>> e54b788 (Adiciona módulo completo de disciplinas e atualiza Admin e Curso para criar disciplinas dentro do curso  com melhorias e ajustes)
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
